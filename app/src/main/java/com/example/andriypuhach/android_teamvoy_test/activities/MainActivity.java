7c731ef628c95141a828e37c3502d57f14693dbf
Andriy Puhach <andriypuhach@Andriys-Mac-mini.local>
2015-01-13 18:30:47 +0200

    callbacks staff

--- app/src/main/java/com/example/andriypuhach/android_teamvoy_test/activities/MainActivity.java
+++ app/src/main/java/com/example/andriypuhach/android_teamvoy_test/activities/MainActivity.java
@@ -5,26 +5,31 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
-import android.widget.*;
+import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.EditText;
+import android.widget.HeaderViewListAdapter;
+import android.widget.ListView;
+import android.widget.TabHost;
+import android.widget.TextView;
+import android.widget.Toast;
 
-
-import com.example.andriypuhach.android_teamvoy_test.MyLogger;
-import com.example.andriypuhach.android_teamvoy_test.asyncs.DownloadDetailsAsync;
-import com.example.andriypuhach.android_teamvoy_test.asyncs.DownloadMoviesAsync;
-import com.example.andriypuhach.android_teamvoy_test.asyncs.SearchMovieAsync;
-import com.example.andriypuhach.android_teamvoy_test.models.Movie;
 import com.example.andriypuhach.android_teamvoy_test.MovieListAdapter;
-//import com.example.andriypuhach.android_teamvoy_test.asyncs.DownloadDetailsAsync;
+import com.example.andriypuhach.android_teamvoy_test.R;
+import com.example.andriypuhach.android_teamvoy_test.models.ImagesResult;
+import com.example.andriypuhach.android_teamvoy_test.models.Movie;
 import com.example.andriypuhach.android_teamvoy_test.models.MovieDetails;
 import com.example.andriypuhach.android_teamvoy_test.models.MovieRequestResult;
+import com.example.andriypuhach.android_teamvoy_test.rest.RestClient;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.listener.PauseOnScrollListener;
 
 import java.util.ArrayList;
-import java.util.concurrent.ExecutionException;
+import java.util.List;
 
-import com.example.andriypuhach.android_teamvoy_test.R;
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
 
 public class MainActivity extends Activity {
     TabHost tabs;
@@ -39,21 +44,39 @@ public class MainActivity extends Activity {
     private OnItemClickListener detailsListener = new OnItemClickListener() {
         @Override
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            Intent intent = new Intent(MainActivity.this, DetailsActivity.class);
+            final Intent intent = new Intent(MainActivity.this, DetailsActivity.class);
             MovieListAdapter adapter = (MovieListAdapter) ((HeaderViewListAdapter) listView.getAdapter()).getWrappedAdapter();
-            Movie movie = adapter.getMovie(position - 1);
-            MovieDetails details = null;
+            final Movie movie = adapter.getMovie(position - 1);
             if (movie.getDetails() == null) {
-                DownloadDetailsAsync task = new DownloadDetailsAsync();
-                try {
-                    details = task.execute(movie.getId()).get();
-                } catch (InterruptedException | ExecutionException e) {
-                    MyLogger.appendLog("TAG", "Interrupted OR Execution Exception in ItemClickListener");
-                }
-                movie.setDetails(details);
+                RestClient.getApi().getDetails(movie.getId(),new Callback<MovieDetails>(){
+                    @Override
+                    public void success(final MovieDetails movieDetails, Response response) {
+                           RestClient.getApi().getImagePathes(movie.getId(),new Callback<ImagesResult>() {
+                               @Override
+                               public void success(ImagesResult imagesResult, Response response) {
+                                   movieDetails.setImagePathes(imagesResult.getPosterPathes());
+                                   movie.setDetails(movieDetails);
+                                   intent.putExtra("Movie", movie);
+                                   startActivity(intent);
+                               }
+
+                               @Override
+                               public void failure(RetrofitError error) {
+                                 Toast.makeText(getApplicationContext(),"have some troubles with ths movie try another one",Toast.LENGTH_LONG);
+                               }
+                           });
+
+                    }
+                    @Override
+                    public void failure(RetrofitError error) {
+
+                    }
+                });
             }
-            intent.putExtra("Movie", movie);
-            startActivity(intent);
+            else{
+                intent.putExtra("Movie",movie);
+                startActivity(intent);
+            }
         }
     };
     //endregion detailsClickListener
@@ -62,53 +85,63 @@ public class MainActivity extends Activity {
         @Override
         public void onTabChanged(String tabId) {
             currentTask = tabId;
-            refreshListByTab(tabId, 1);
+            refreshListByTab(1);
         }
     };
 
 
     void refreshListBySearch(String search) {
-        SearchMovieAsync searchMovieAsync = new SearchMovieAsync();
-        try {
-            MovieRequestResult result = searchMovieAsync.execute(search, String.valueOf(currentPage)).get();
-            if (result.getResults() != null) {
-                currentPage = result.getPage();
-                totalPages = result.getTotal_pages();
-                ArrayList<Movie> movies = (ArrayList<Movie>) result.getResults();
-                listAdapter.setMovies(movies);
+        RestClient.getApi().search(search,currentPage,new Callback<MovieRequestResult>(){
+            @Override
+            public void success(MovieRequestResult result, Response response) {
+                if (result.getResults() != null) {
+                    currentPage = result.getPage();
+                    totalPages = result.getTotal_pages();
+                    ArrayList<Movie> movies = (ArrayList<Movie>) result.getResults();
+                    listAdapter.setMovies(movies);
+                    ((TextView) header.findViewById(R.id.currentPageView)).setText(currentPage + " of " + totalPages);
+                    if (listView.getHeaderViewsCount() == 0)
+                        listView.addHeaderView(header);
+                    listView.setAdapter(listAdapter);
+                }
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+                currentPage=0;
+                totalPages=0;
+                listAdapter.setMovies(new ArrayList<Movie>());
                 ((TextView) header.findViewById(R.id.currentPageView)).setText(currentPage + " of " + totalPages);
                 if (listView.getHeaderViewsCount() == 0)
                     listView.addHeaderView(header);
                 listView.setAdapter(listAdapter);
             }
-        } catch (InterruptedException | ExecutionException e) {
-            e.printStackTrace();
-
-        } finally {
-            searchMovieAsync.cancel(true);
-        }
+        });
     }
 
 
-    void refreshListByTab(String tabId, int page) {
-        DownloadMoviesAsync rlistAsyncTask = new DownloadMoviesAsync();
-        try {
-            MovieRequestResult result = rlistAsyncTask.execute(tabId, String.valueOf(page)).get();
-            currentPage = result.getPage();
-            totalPages = result.getTotal_pages();
-            ArrayList<Movie> movies = (ArrayList<Movie>) result.getResults();
-            listAdapter.setMovies(movies);
-            ((TextView) header.findViewById(R.id.currentPageView)).setText(currentPage + " of " + totalPages);
-            if (listView.getHeaderViewsCount() == 0)
-                listView.addHeaderView(header);
-            listView.setAdapter(listAdapter);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        } finally {
-            rlistAsyncTask.cancel(true);
-        }
+    void refreshListByTab(int page) {
+        RestClient.getApi().getMovies(currentTask,currentPage,new Callback<MovieRequestResult>() {
+            @Override
+            public void success(MovieRequestResult movieRequestResult, Response response) {
+                if(movieRequestResult.getResults()!=null) {
+                    currentPage = movieRequestResult.getPage();
+                    totalPages = movieRequestResult.getTotal_pages();
+                    listAdapter.setMovies((ArrayList<Movie>) movieRequestResult.getResults());
+                    ((TextView) header.findViewById(R.id.currentPageView)).setText(currentPage + " of " + totalPages);
+                    if (listView.getHeaderViewsCount() == 0)
+                        listView.addHeaderView(header);
+                    if (listView.getHeaderViewsCount() == 0)
+                        listView.addHeaderView(header);
+                    listView.setAdapter(listAdapter);
+                }
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+                Toast.makeText(getApplicationContext(),"trouble",Toast.LENGTH_LONG);
+            }
+        });
     }
 
     View createHeader() {
@@ -120,7 +153,7 @@ public class MainActivity extends Activity {
                 if (currentPage < totalPages) {
                     currentPage++;
                     if (currentTask != "search")
-                        refreshListByTab(currentTask, currentPage);
+                        refreshListByTab(currentPage);
                     else
                        refreshListBySearch(searchView.getText().toString());
                 }
@@ -133,7 +166,7 @@ public class MainActivity extends Activity {
                 if (currentPage > 1) {
                     currentPage--;
                     if (currentTask != "search")
-                        refreshListByTab(currentTask, currentPage);
+                        refreshListByTab(currentPage);
                     else
                         refreshListBySearch(searchView.getText().toString());
                 }
@@ -156,7 +189,7 @@ public class MainActivity extends Activity {
                 if (event != null && event.getAction() == KeyEvent.ACTION_DOWN && event.getKeyCode() == KeyEvent.KEYCODE_ENTER) {
                     if (searchView.getText().toString().equals("")) {
                         currentTask = tabs.getCurrentTabTag();
-                        refreshListByTab(currentTask, 1);
+                        refreshListByTab(1);
                     } else {
                         currentTask = "search";
                         refreshListBySearch(Uri.encode(searchView.getText().toString()));
--- app/src/main/java/com/example/andriypuhach/android_teamvoy_test/services/RetrofitMovieService.java
+++ app/src/main/java/com/example/andriypuhach/android_teamvoy_test/services/RetrofitMovieService.java
@@ -1,5 +1,7 @@
 package com.example.andriypuhach.android_teamvoy_test.services;
 
+import android.media.ImageReader;
+
 import com.example.andriypuhach.android_teamvoy_test.models.ImagesResult;
 import com.example.andriypuhach.android_teamvoy_test.models.Movie;
 import com.example.andriypuhach.android_teamvoy_test.models.MovieDetails;
@@ -7,6 +9,7 @@ import com.example.andriypuhach.android_teamvoy_test.models.MovieRequestResult;
 
 import java.util.List;
 
+import retrofit.Callback;
 import retrofit.http.GET;
 import retrofit.http.Path;
 import retrofit.http.Query;
@@ -17,12 +20,12 @@ import retrofit.http.Query;
 public interface RetrofitMovieService {
     static final String apiKey = "a37df950b3c1b256fded657ae4562b93";
     @GET("/movie/{thing}?api_key="+apiKey)
-    public MovieRequestResult getMovies(@Path("thing")String thing,@Query("page")int page);
+    public void getMovies(@Path("thing")String thing,@Query("page")int page, Callback<MovieRequestResult> movieRes);
     @GET("/movie/{id}?api_key="+apiKey)
-    public MovieDetails getDetails(@Path("id")int id);
+    public void getDetails(@Path("id")int id,Callback<MovieDetails> det);
     @GET("/movie/{id}/images?api_key="+apiKey)
-    public ImagesResult getImagePathes(@Path("id")int id);
+    public void getImagePathes(@Path("id")int id,Callback<ImagesResult> imRes);
     @GET("/search/movie?api_key="+apiKey)
-    public MovieRequestResult search(@Query("query")String query,@Query("page")int page);
+    public void search(@Query("query")String query,@Query("page")int page,Callback<MovieRequestResult> movieRes);
 
 }
